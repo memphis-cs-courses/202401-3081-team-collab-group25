eae1d0500f07968b8216e7b5d51ba7192f75439c
Jonnie Nguyen <jonnienguyen17@gmail.com>
2024-04-22 13:25:09 -0500

    Add jonnienguyen page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
+
+  get "jonnienguyen", to: "pages#jonnienguyen", as: "jonnienguyen"
 end
